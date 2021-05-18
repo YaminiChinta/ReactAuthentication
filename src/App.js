a4df16b70fbf6e20feed57f54b05d000935bb70c
YaminiChinta <67141828+YaminiChinta@users.noreply.github.com>
2021-05-18 16:31:58 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Sidebar from './components/Sidebar'
 import Navbar from './components/Navbar'
 import Content from './components/Content'
-
+import  './components/Auth.css'
 function App() {
   return (
     <div style={{display:"flex"}} >
